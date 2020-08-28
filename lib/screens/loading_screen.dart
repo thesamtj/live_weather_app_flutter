7097a4fd26f3f4e24940067650e062492a392310
Samuel Tijani <omalitijani@gmail.com>
2020-08-28 18:09:09 +0100

    Get location & print to console

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -10,6 +10,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
   }
 
   @override
@@ -19,6 +20,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
