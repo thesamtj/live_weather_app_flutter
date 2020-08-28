71e5e93f1956641afffea15608be2eb261de99a5
Samuel Tijani <omalitijani@gmail.com>
2020-08-28 17:51:09 +0100

    Get location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,6 +7,11 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
