974d4c9bbd1297008e2597d37081910607c90613
Samuel Tijani <omalitijani@gmail.com>
2021-12-29 18:58:31 +0100

    Get value

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -76,7 +76,21 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                      if (typedName != null) {
+                        var weatherData =
+                            await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
