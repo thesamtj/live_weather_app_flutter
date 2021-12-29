09a95c9a1592f6ae6af4ce784401cf38eb251671
Samuel Tijani <omalitijani@gmail.com>
2021-12-29 18:47:10 +0100

    Pass value

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -48,7 +48,9 @@ class _CityScreenState extends State<CityScreen> {
                 ),
               ),
               FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: Text(
                   'Get Weather',
                   style: kButtonTextStyle,
